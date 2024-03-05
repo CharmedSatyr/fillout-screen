d9d375262a134fc49657f7fa9e59669ea9176ebf
Joseph <electronic@mailbox.org>
2024-03-05 15:09:44 -0800

    Add prettier, fix scripts

--- src/routes/root.test.ts
+++ src/routes/root.test.ts
@@ -1,15 +1,15 @@
-import {Request, Response} from "express";
-import {getRoot} from "~/routes/root";
+import { Request, Response } from "express";
+import { getRoot } from "~/routes/root";
 
 describe("~/routes/root", () => {
-    describe("GET /", () => {
-        it("should return a 200 response and a string body", async () => {
-            const req = {} as Request;
-            const res = { send: jest.fn() } as unknown as Response;
+  describe("GET /", () => {
+    it("should return a 200 response and a string body", async () => {
+      const req = {} as Request;
+      const res = { send: jest.fn() } as unknown as Response;
 
-            getRoot(req, res);
+      getRoot(req, res);
 
-            expect(res.send).toHaveBeenCalledWith("Hello, Fillout!");
-        });
+      expect(res.send).toHaveBeenCalledWith("Hello, Fillout!");
     });
+  });
 });
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 export const getRoot = (_: Request, res: Response) => {
-    res.send("Hello, Fillout!");
+  res.send("Hello, Fillout!");
 };
 
 router.get("/", getRoot);
